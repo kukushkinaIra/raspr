3b48db1a60213fce475942c4eaeb2f8f1d92c976
irina.kukushkina.21.05.2001@gmail.com <irina.kukushkina.21.05.2001@gmail.com>
2023-06-28 09:28:42 +0300

    add nex sections on main page

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -5,6 +5,12 @@ import Service from "./parts/Service.jsx"
 import Prices from "./parts/Prices.jsx"
 import Reviews from "./parts/Reviews.jsx"
 import Footer from "../footer/Footer"
+import AboutUs from "./parts/AboutUs.jsx"
+import ReferalProgramm from "./parts/ReferalProgramm.jsx"
+import Team from "./parts/Team.jsx"
+import Vacansies from "./parts/Vacansies.jsx"
+
+
 
 function MainPage() {
     return (
@@ -12,8 +18,12 @@ function MainPage() {
         <div className="main">
           <Navigation/>
           <MainInfo/>
+          <AboutUs/>
           <Service/>
           <Prices/>
+          <ReferalProgramm/>
+          <Team/>
+          <Vacansies/>
           <Reviews/>
           <Footer/>
         </div>
---
+++ src/components/mainPage/parts/Team.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+import main from "./images/alex.jpg"
+
+function Team(){
+    return(
+        <section className="about_us team">
+            <div className="container main_container">
+                 <h2>Наша Команда</h2>
+                <div className="main_description main_description_team">
+                    <div className="team_info">
+                        <p>
+                            Алексей Гуревич - руководитель данного проекта.
+                            Алексей - успешный предприниматель с многолетним опытом оптимизации бизнеса международного формата.
+                            Он помогает сохранить и приумножить деньги предпринимателям и не только. А также сохраняет ваши нервы и ваше время во время отработки. Алексей уже изменил в лучшую сторону не одну жизнь, изменит и твою!
+                        </p>
+                        <img className="mainImg" src={main} />
+                    </div>
+                    {/* <div>
+                        <img className="mainImg" src={main} />
+                    </div> */}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Team
